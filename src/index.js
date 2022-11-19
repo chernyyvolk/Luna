69077001bf4adf94d29f11da4b4bc618f430e914
Chernyy Volk <85821992+chernyyvolk@users.noreply.github.com>
2022-11-19 04:10:54 -0500

    added slash command registration and Rest

--- src/index.js
+++ src/index.js
@@ -1,15 +1,42 @@
 import { config } from "dotenv";
-import { Client, GatewayIntentBits } from "discord.js";
+import { Client, GatewayIntentBits, Routes } from "discord.js";
+import { REST } from "@discordjs/rest";
+
 config();
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+const TOKEN = process.env.BOT_TOKEN;
+const CLIENT_ID = process.env.CLIENT_ID;
+const GUILD_ID = process.env.GUILD_ID;
 
-client.login(process.env.BOT_TOKEN);
+const client = new Client({ intents: [
+  GatewayIntentBits.Guilds,
+  GatewayIntentBits.GuildMessages,
+  GatewayIntentBits.MessageContent
+],
+ });
 
-client.on("ready", () => {
-  console.log(`${client.user.tag}/(${client.user.id}) has initialized. Awaiting operating parameters...`);
-})
+ const rest = new REST({ version: '10'}).setToken(TOKEN);
 
-client.on('messageCreate', (message) => {
-  console.log(message.content);
-})
+client.on("ready", () => { console.log(`${client.user.tag}/(${client.user.id}) has initialized. Awaiting operating parameters...`); })
+
+async function main() {
+
+const commands = [
+  {
+    name: 'ping',
+    description: 'Pong',
+  },
+];
+
+  try{
+    console.log('Started refreshing application (/) commands.');
+    await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {
+      body: commands,
+    });
+    client.login(TOKEN);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+main();
