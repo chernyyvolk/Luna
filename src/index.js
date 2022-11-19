c06ab8e20f359381235e51e1f0c9d2a7dbd2e6e3
Chernyy Volk <85821992+chernyyvolk@users.noreply.github.com>
2022-11-19 03:44:43 -0500

    added GatewayIntentBits

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import { config } from "dotenv";
-import { Client } from "discord.js";
+import { Client, GatewayIntentBits } from "discord.js";
 config();
 
-const client = new Client({ intents: ['Guilds', 'GuildMessages'] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
 client.login(process.env.BOT_TOKEN);
 
 client.on("ready", () => {
-  console.log("bot is logged in");
+  console.log(`${client.user.tag}/(${client.user.id}) has initialized. Awaiting operating parameters...`);
+})
+
+client.on('messageCreate', (message) => {
+  console.log(message.content);
 })
