d452cf8a093fa8e3cbd8e9ef87ea67a8be061d89
Chernyy Volk <85821992+chernyyvolk@users.noreply.github.com>
2022-11-19 01:59:54 -0500

    init

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import { config } from 'dotenv';
-import { Client } from 'discord.js';
-config();
-
-const client = new Client({ intents: ['Guilds', 'GuildMessages'] });
-
-client.login(process.env.BOT_TOKEN);
-
-
